ee1d7182868af8e64c355093e5ae50ecc6108cad
Khairil Kasimir Isa <ranger.karl@gmail.com>
2025-06-15 00:26:29 +0800

    randomize textures!

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -13,10 +13,19 @@ public partial class Asteroid : Area2D
 
     public override void _Ready()
     {
+        
+
         Random rnd = new Random();
         var x = rnd.Next(-1, 1);
         var y = rnd.Next(-1, 1);
         Direction = new Vector2(x, y);
+
+        // update the Sprite2D with a random asset from the image paths
+
+        Sprite2D sprite = (Sprite2D)this.GetNode("./Sprite2D");
+        var img = (Texture2D)GD.Load(imagePaths[rnd.Next(0, 3)]);
+        sprite.Texture = img;
+
     }
 
     public override void _Process(double delta)
